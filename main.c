ef43dd9f35a49320bb945b5c9a62a32f7506d20b
fathom820 <michaelfrank2002@gmail.com>
2021-11-17 16:06:52 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+ * Michael Frank
+ * Lab 5: MIPS Assembler
+ * Dr. Sangkook Lee
+ * CMPE 220 - 02
+ * Created 11/17/2021
+ * Modified 11/17/2021
+ */
+
+#include <stdio.h>
+
+char *directives[] = {
+        ".data",
+        ".text",
+        ".word"
+};
+
+int main(int argc, char *argv[]) {
+
+    char *fname = NULL;
+    FILE *file = NULL;
+    return 0;
+}
